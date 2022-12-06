48ed189ceeb09a0be5939426b5200f0dda80034d
Paul Mana <paul@malikaz.com>
2022-12-05 22:16:30 -0700

    question 6 part two

--- advent_of_code_2022/src/main.rs
+++ advent_of_code_2022/src/main.rs
@@ -18,16 +18,27 @@ fn main() {
 
 fn problem_06(){
     let data: String = load_string("src/assets/problem_06");
-    let mut count: usize = 4;
+    let mut count: usize = 14;
 
-    for item in data.as_bytes().to_vec().windows(4){
+    for item in data.as_bytes().to_vec().windows(14){
         let mut temp: HashSet<u8> = HashSet::new();
         temp.insert(item[0]);
         temp.insert(item[1]);
         temp.insert(item[2]);
         temp.insert(item[3]);
+        temp.insert(item[4]);
+        temp.insert(item[5]);
+        temp.insert(item[6]);
+        temp.insert(item[7]);
+        temp.insert(item[8]);
+        temp.insert(item[9]);
+        temp.insert(item[10]);
+        temp.insert(item[11]);
+        temp.insert(item[12]);
+        temp.insert(item[13]);
+
         
-        if temp.len() == 4 {
+        if temp.len() == 14 {
             break;
         }
         count = count + 1;
