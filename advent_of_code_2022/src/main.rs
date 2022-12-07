03496e0c4b6b17d459da736e47054a502dd354a7
Paul Mana <paul@malikaz.com>
2022-12-06 23:58:49 -0700

    test data work but not real codes a mess done for now

--- advent_of_code_2022/src/main.rs
+++ advent_of_code_2022/src/main.rs
@@ -5,6 +5,7 @@ use std::io::prelude::*;
 use std::ops::Index;
 
 fn main() {
+    problem_07();
     problem_06();
     problem_05();
     //problem_02();
@@ -13,6 +14,80 @@ fn main() {
     //problem_01();
     
     
+    
+}
+
+#[derive(Debug)]
+struct FileNode {
+    parent: usize,
+    value: String,
+    size: usize,
+    children: Vec<usize>
+}
+
+fn problem_07(){
+    let data: String = load_string("src/assets/problem_07");
+    let mut answer: usize = 0;
+    let mut my_vec: Vec<FileNode> = Vec::new();
+
+    let mut current_position: usize = 0;
+
+    for line in data.split("\n"){
+        if line.contains("cd") {
+            if line.contains(".."){
+                current_position = my_vec[current_position].parent;
+                continue;
+            }else {
+                let line = line.replace("$ cd", "");
+                let line = line.trim();
+                my_vec.push(FileNode{parent:current_position, size:0, value:String::from(line), children: Vec::new()});
+                current_position = my_vec.len() -1;
+                continue;
+            }
+            
+        }
+        if line.contains("dir") {
+            my_vec.push(FileNode{parent:current_position, size:0, value:String::from(line), children: Vec::new()});
+            continue;
+        }
+        if line == "$ ls" {
+            continue;
+        }
+        // add child
+        let mut temp_child: Vec<&str> = Vec::new();
+        for child_part in line.split(" ") {
+            temp_child.push(child_part);
+        }
+        my_vec.push(FileNode { parent: current_position, value:String::from(temp_child[1]), size: temp_child[0].parse::<usize>().unwrap(), children: Vec::new() });
+        
+        let current_len = my_vec.len();
+        
+        my_vec[current_position].children.push(current_len -1)
+    }
+    // fix children
+
+    for item in 0..my_vec.len(){
+        let parent = my_vec[item].parent;
+        let mut children = my_vec[item].children.clone();
+        my_vec[parent].children.append(&mut children);
+    }
+    
+
+    for item in my_vec.iter(){
+        if item.children.len() > 0 {
+            let mut temp_size: usize = 0;
+            for child in item.children.iter(){
+                 temp_size = temp_size + my_vec[*child].size;
+            }
+            if temp_size < 100_001 {
+                answer = answer + temp_size;
+            }
+        }
+    }
+    for item in my_vec.iter(){
+        print!("{:?}\n", item)
+    }
+    print!("Problems 7 answer: {:?}\n", answer)
     
 }
 
@@ -36,7 +111,7 @@ fn problem_06(){
         temp.insert(item[11]);
         temp.insert(item[12]);
         temp.insert(item[13]);
-
+        
         
         if temp.len() == 14 {
             break;
