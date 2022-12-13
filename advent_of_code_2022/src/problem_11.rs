ea61c28ad46f9ebd7b6112cbfaabf443be488d0d
Paul Mana <paul@malikaz.com>
2022-12-12 22:12:33 -0700

    woo

--- advent_of_code_2022/src/problem_11.rs
+++ advent_of_code_2022/src/problem_11.rs
@@ -1,18 +1,19 @@
+use std::hash::{Hash, self};
 use std::{fs::read_to_string, ops::RangeBounds};
-extern crate yaml_rust;
-use yaml_rust::YamlLoader;
+use std::collections::HashMap;
 
+#[derive(Debug)]
 struct Monkey {
-    items: Vec<i32>,
-    test: i32,
+    items: Vec<i64>,
+    test: i64,
     operation: Operation,
-    opertaion_value: i32,
-    if_true: i32,
-    if_false: i32,
+    opertaion_value: i64,
+    if_true: i64,
+    if_false: i64,
 }
 
 impl Monkey {
-    fn new(test: i32, if_true: i32, if_false: i32, operation: Operation, opertaion_value: i32, items:Vec<i32>) -> Monkey{
+    fn new(test: i64, if_true: i64, if_false: i64, operation: Operation, opertaion_value: i64, items:Vec<i64>) -> Monkey{
         return Monkey { items: items, test: test, if_true: if_true, operation:operation, opertaion_value:opertaion_value, if_false:if_false }
     }
 
@@ -26,19 +27,74 @@ impl Monkey {
         }
     }
 }
-
+#[derive(Debug)]
 enum Operation{
     add,
     subtract,
     divide,
-    multiply
+    multiply,
+    old
 }
 
 pub fn problem_11() {
-    let data = &read_to_string("src/assets/problem_11").unwrap();
-
-    let mut monkeys: Vec<Monkey> = Vec::new();
     
+    let data = &read_to_string("src/assets/problem_11").unwrap();
+    let mut monkeys: Vec<Monkey> = parse_monkeys(data);
+    let mut monkey_activity:Vec<i64> = seed_activity_vec(&monkeys);
+    let common_divisor: i64 = get_common_divisor(&monkeys);
+    
+    for _i in 0..10_000{
+        for monkey in 0..monkeys.len() {
+            for item in 0..monkeys[monkey].items.len() {
+                let temp_activity = monkey_activity[monkey];
+                monkey_activity[monkey] = temp_activity + 1;
+
+                match monkeys[monkey].operation {
+                    Operation::add => monkeys[monkey].items[item] = monkeys[monkey].items[item] + monkeys[monkey].opertaion_value,
+                    Operation::subtract => monkeys[monkey].items[item] = monkeys[monkey].items[item] - monkeys[monkey].opertaion_value,
+                    Operation::multiply => monkeys[monkey].items[item] = monkeys[monkey].items[item] * monkeys[monkey].opertaion_value,
+                    Operation::divide => monkeys[monkey].items[item] = monkeys[monkey].items[item] / monkeys[monkey].opertaion_value,
+                    Operation::old => monkeys[monkey].items[item] = monkeys[monkey].items[item] * monkeys[monkey].items[item]
+                }
+
+
+                
+                monkeys[monkey].items[item] = monkeys[monkey].items[item] % common_divisor;
+
+                if monkeys[monkey].items[item] % monkeys[monkey].test == 0 {
+                    let index = monkeys[monkey].if_true as usize;
+                    let value = monkeys[monkey].items[item];
+                    monkeys[index].items.push(value);
+
+                } else {
+                    let index = monkeys[monkey].if_false as usize;
+                    let value = monkeys[monkey].items[item];
+                    monkeys[index].items.push(value);
+                }
+            }
+            // remove all items from this monkey
+            for _i in 0..monkeys[monkey].items.len() {
+                monkeys[monkey].items.pop();
+            }
+            
+        }
+    }
+   
+    monkey_activity.sort_by(|a, b| b.cmp(a));
+    print!("Problem 11: {:?}\n\n", monkey_activity)
+}
+
+fn seed_activity_vec(monkey: &Vec<Monkey>) -> Vec<i64>{
+    let mut vec: Vec<i64> = Vec::new();
+    for _i in 0..monkey.len(){
+        vec.push(0);
+    }
+    vec
+}
+
+fn parse_monkeys(data: &String) -> Vec<Monkey>{
+    let mut monkeys: Vec<Monkey> = Vec::new();
+    let mut first_run: bool = true;
     let mut temp_monkey = Monkey {
         items: Vec::new(),
                 operation: Operation::add,
@@ -49,7 +105,7 @@ pub fn problem_11() {
     };
 
     for line in data.split("\n") {
-        if line.contains("Monkey") {
+        if line.contains("Monkey") && !first_run {
             monkeys.push(temp_monkey);
             temp_monkey = Monkey {
                 items: Vec::new(),
@@ -63,39 +119,52 @@ pub fn problem_11() {
             continue;
         }
         if line.contains("Starting items") {
-            let line = line.replace(",", "");
             let line = line.replace("Starting items: ", "");
-            for sub_line in line.split(" "){
-                temp_monkey.items.push(sub_line.parse::<i32>().unwrap());
+            let line = line.replace(" ", "");
+            for sub_line in line.split(","){
+                temp_monkey.items.push(sub_line.parse::<i64>().unwrap());
             }
 
         }
         else if line.contains("Operation") {
             let line = line.replace("Operation: new = old ", "");
+            let line = line.trim();
             for sub_item in line.split(" "){
                 match sub_item {
                     "+" => temp_monkey.operation = Operation::add,
                     "-" => temp_monkey.operation = Operation::subtract,
                     "*" => temp_monkey.operation = Operation::multiply,
                     "/" => temp_monkey.operation = Operation::divide,
+                    "old" => temp_monkey.operation = Operation::old,
                     _ => temp_monkey.opertaion_value = sub_item.parse().unwrap()
                 }
             }
         }
         else if line.contains("Test") {
             let line = line.replace("Test: divisible by ", "");
+            let line = line.trim();
             temp_monkey.test = line.parse().unwrap();
         }
         else if line.contains("If true") {
             let line = line.replace("If true: throw to monkey ", "");
+            let line = line.trim();
             temp_monkey.if_true = line.parse().unwrap();
         }
         else if line.contains("If false") {
             let line = line.replace("If false: throw to monkey ", "");
+            let line = line.trim();
             temp_monkey.if_false = line.parse().unwrap();
         }
-
+        first_run = false;
     }
+    monkeys.push(temp_monkey);
+    monkeys
+}
 
-    print!("Problem 11: {:?}\n\n", monkeys)
+fn get_common_divisor(monkeys: &Vec<Monkey>) -> i64{
+    let mut temp: i64 = 1;
+    for m in monkeys.iter(){
+        temp = temp * m.test;
+    }
+    temp
 }
