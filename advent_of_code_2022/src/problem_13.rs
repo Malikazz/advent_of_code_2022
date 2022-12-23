88fb87d954f755b71ea46faabfbb651d88ff649d
Paul Mana <paul@malikaz.com>
2022-12-22 19:21:38 -0700

    problem 13 seems really meh gonna skip for now

--- advent_of_code_2022/src/problem_13.rs
+++ advent_of_code_2022/src/problem_13.rs
@@ -2,6 +2,11 @@ use std::fs::read_to_string;
 pub fn problem_13() {
     let data: String = read_to_string("src/assets/problem_13_test").unwrap();
 
+    // if values are int left should be lower
+
+    // if values are both lists compare left to right for size
+
+    // if one value is int and other is list convert int to list
 
 
 
---
+++ advent_of_code_2022/src/problem_14.rs
@@ -0,0 +1,19 @@
+use std::fs::read_to_string;
+pub fn problem_14() {
+    
+
+   // sand trys to fall left then right, can fall left or right multiple levels 
+
+
+
+    print!("Problem 14: {:?}\n\n", data);
+}
+
+fn create_map() -> Vec<Vec<i32>> {
+    let map:Vec<Vec<i32>> = Vec::new();
+    let data = read_to_string("src/assets/problem_14_test").unwrap().split('\n');
+    for line in data.iter() {
+        
+    }
+    return map
+}
